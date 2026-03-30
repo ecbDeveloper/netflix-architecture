1a9783cce28990fff7c32da36f43533d1239ab61
ecbDeveloper <developer.ecb@gmail.com>
2026-03-30 12:35:46 -0300

    refactor: change column role from users table to role_id

--- internal/database/migrations/010_add_user_role_column_on_users_table.sql
+++ internal/database/migrations/010_add_user_role_column_on_users_table.sql
@@ -1,11 +1,12 @@
 -- Write your migrate up statements here
 ALTER TABLE users 
-ADD COLUMN role INT NOT NULL DEFAULT 2,
-ADD CONSTRAINT users_roles_user_fk FOREIGN KEY (role)
-  REFERENCES users_roles(id);
+ADD COLUMN role_id INT NOT NULL DEFAULT 2,
+ADD CONSTRAINT users_roles_user_fk FOREIGN KEY (role_id)
+  REFERENCES users_roles(id)
+  ON UPDATE CASCADE;
 ---- create above / drop below ----
 ALTER TABLE users
   DROP CONSTRAINT IF EXISTS users_roles_user_fk,
-  DROP COLUMN IF EXISTS role; 
+  DROP COLUMN IF EXISTS role_id; 
 -- Write your migrate down statements here. If this migration is irreversible
 -- Then delete the separator line above.
