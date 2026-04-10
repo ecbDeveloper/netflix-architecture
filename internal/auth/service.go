78e85174e1eaaf69065701c1f9f236f7991a5cc6
ecbDeveloper <developer.ecb@gmail.com>
2026-04-10 04:52:08 -0300

    refactor: add validation to verifiy password size

--- internal/auth/service.go
+++ internal/auth/service.go
@@ -2,14 +2,12 @@ package auth
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"strings"
 
 	"github.com/ecbDeveloper/netflix-architecture/internal/apperror"
 	"github.com/ecbDeveloper/netflix-architecture/internal/database/sqlc"
 	"github.com/ecbDeveloper/netflix-architecture/internal/graph/model"
-	"github.com/jackc/pgx/v5"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -35,14 +33,16 @@ func (s *ServiceImpl) Login(ctx context.Context, input model.LoginInput) (sqlc.U
 		return sqlc.User{}, &apperror.ValidationError{Field: "password", Message: "password is required"}
 	}
 
+	if len(input.Password) < 8 {
+		return sqlc.User{}, &apperror.ValidationError{Field: "password", Message: "password must be at least 8 characters"}
+	}
+	if len(input.Password) > 72 {
+		return sqlc.User{}, &apperror.ValidationError{Field: "password", Message: "password must be at most 72 characters"}
+	}
+
 	user, err := s.queries.GetUserByEmail(ctx, input.Email)
 	if err != nil {
-		if errors.Is(err, pgx.ErrNoRows) {
-			return sqlc.User{}, &apperror.NotFoundError{Entity: "user"}
-		}
-
 		return sqlc.User{}, fmt.Errorf("failed to fetch user by email from database: %w", err)
-
 	}
 
 	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(input.Password)); err != nil {
