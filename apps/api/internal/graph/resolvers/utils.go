f30875a4a946c75decdb9fa89eeeb3e8e354943d
ecbDeveloper <developer.ecb@gmail.com>
2026-04-13 02:59:41 -0300

    fix: session data retrieves functions

--- apps/api/internal/graph/resolvers/utils.go
+++ apps/api/internal/graph/resolvers/utils.go
@@ -9,7 +9,7 @@ import (
 )
 
 func (r *Resolver) getUserIDFromSession(ctx context.Context) (uuid.UUID, error) {
-	userID, ok := ctx.Value(shared.SessionUserIDKey).(uuid.UUID)
+	userID, ok := r.Sessions.Get(ctx, shared.SessionUserIDKey).(uuid.UUID)
 	if !ok {
 		return uuid.Nil, &apperror.UnauthorizedError{Message: ErrUserNotLoggedIn.Error()}
 	}
@@ -17,7 +17,7 @@ func (r *Resolver) getUserIDFromSession(ctx context.Context) (uuid.UUID, error) 
 }
 
 func (r *Resolver) getUserRoleIDFromSession(ctx context.Context) (int32, error) {
-	roleID, ok := ctx.Value(shared.SessionRoleIDKey).(int32)
+	roleID, ok := r.Sessions.Get(ctx, shared.SessionRoleIDKey).(int32)
 	if !ok {
 		return 0, &apperror.UnauthorizedError{Message: ErrUserNotLoggedIn.Error()}
 	}
@@ -25,9 +25,9 @@ func (r *Resolver) getUserRoleIDFromSession(ctx context.Context) (int32, error) 
 }
 
 func (r *Resolver) getProfileIDFromSession(ctx context.Context) (uuid.UUID, error) {
-	profileID, ok := ctx.Value(shared.SessionProfileIDKey).(uuid.UUID)
+	profileID, ok := r.Sessions.Get(ctx, shared.SessionProfileIDKey).(uuid.UUID)
 	if !ok {
-		return uuid.Nil, &apperror.UnauthorizedError{Message: ErrUserNotLoggedIn.Error()}
+		return uuid.Nil, &apperror.ForbiddenError{Message: "you must select a profile to access this content"}
 	}
 	return profileID, nil
 }
