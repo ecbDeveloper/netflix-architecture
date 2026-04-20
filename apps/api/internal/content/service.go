ecfc035f6d1cd554a835958843c867f5c9013bf1
ecbDeveloper <developer.ecb@gmail.com>
2026-04-20 13:12:34 -0300

    chore: delete content service comments

--- apps/api/internal/content/service.go
+++ apps/api/internal/content/service.go
@@ -31,37 +31,30 @@ func (s *ServiceImpl) CreateContent(ctx context.Context, input model.CreateConte
 	panic(fmt.Errorf("not implemented: CreateContent - createContent"))
 }
 
-// UpdateContent is the resolver for the updateContent field.
 func (s *ServiceImpl) UpdateContent(ctx context.Context, id uuid.UUID, input model.UpdateContentInput) (*model.Content, error) {
 	panic(fmt.Errorf("not implemented: UpdateContent - updateContent"))
 }
 
-// DeleteContent is the resolver for the deleteContent field.
 func (s *ServiceImpl) DeleteContent(ctx context.Context, id uuid.UUID) (bool, error) {
 	panic(fmt.Errorf("not implemented: DeleteContent - deleteContent"))
 }
 
-// GetContent is the resolver for the getContent field.
 func (s *ServiceImpl) GetContent(ctx context.Context, id uuid.UUID) (*model.Content, error) {
 	panic(fmt.Errorf("not implemented: GetContent - getContent"))
 }
 
-// ListContents is the resolver for the listContents field.
 func (s *ServiceImpl) ListContents(ctx context.Context) ([]*model.Content, error) {
 	panic(fmt.Errorf("not implemented: ListContents - listContents"))
 }
 
-// ListKidsContents is the resolver for the listKidsContents field.
 func (s *ServiceImpl) ListKidsContents(ctx context.Context) ([]*model.Content, error) {
 	panic(fmt.Errorf("not implemented: ListKidsContents - listKidsContents"))
 }
 
-// ListContentsByType is the resolver for the listContentsByType field.
 func (s *ServiceImpl) ListContentsByType(ctx context.Context, contentType model.ContentType) ([]*model.Content, error) {
 	panic(fmt.Errorf("not implemented: ListContentsByType - listContentsByType"))
 }
 
-// ListContentsByGenre is the resolver for the listContentsByGenre field.
 func (s *ServiceImpl) ListContentsByGenre(ctx context.Context, genreID int32) ([]*model.Content, error) {
 	panic(fmt.Errorf("not implemented: ListContentsByGenre - listContentsByGenre"))
 }
