2d3797917dd190221a33575d9020a5f59b2c7eb0
ecbDeveloper <developer.ecb@gmail.com>
2026-04-22 15:43:04 -0300

    feat: add DeleteFile function on storage services

--- apps/api/internal/storage/service.go
+++ apps/api/internal/storage/service.go
@@ -10,6 +10,7 @@ import (
 
 type Service interface {
 	Upload(ctx context.Context, fileName string, file io.Reader) (string, error)
+	DeleteFile(ctx context.Context, filePath string) error
 }
 
 type ServiceImpl struct {
@@ -31,7 +32,19 @@ func (s *ServiceImpl) Upload(ctx context.Context, fileName string, file io.Reade
 	}
 	defer dst.Close()
 
-	io.Copy(dst, file)
+	_, err = io.Copy(dst, file)
+	if err != nil {
+		return "", fmt.Errorf("failed to copy file: %w", err)
+	}
 
 	return contentURL, nil
+}
+
+func (s *ServiceImpl) DeleteFile(ctx context.Context, contentURL string) error {
+	err := os.Remove(contentURL)
+	if err != nil {
+		return fmt.Errorf("failed to remove file: %w", err)
+	}
+
+	return nil
 }
